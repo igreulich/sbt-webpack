de027cad4cf65b73867c2482a394d25d93651553
Ian Greulich <ian@greulich.me>
2019-07-10 12:02:43 -0400

    create typical webpack config, enabled code-splitting, and external sourcemaps in production mode

--- test-play-project/webpack.config.js
+++ test-play-project/webpack.config.js
@@ -1,40 +1,92 @@
+const webpack = require('webpack');
+const flowright = require('lodash.flowright');
+const TerserPlugin = require('terser-webpack-plugin');
 const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
 
-const baseConfig = {
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          chunks: 'initial',
-          name: 'vendor',
-          enforce: true,
-        }
-      }
+const nodeEnv = process.env.NODE_ENV || '';
+
+function generateBaseConfig() {
+  const config = {
+    mode: '',
+    devtool: '',
+    resolve: {
+      extensions: ['.js', '.jsx'],
     },
-  },
-  plugins: [
-    new MomentLocalesPlugin({
-      localesToKeep: ['es-us'],
-    }),
-  ],
-};
-
-function configFactory(base) {
-  if (process.env.NODE_ENV === 'production') {
-    console.log('[sbt-webpack] Enable the production mode');
-
-    base.mode = 'production';
-  } else {
-    console.log('[sbt-webpack] Enable the development mode');
-
-    base.mode = 'development';
+    module: {},
+    // We never need all of the moment locales.
+    plugins: [
+      new MomentLocalesPlugin({
+        localesToKeep: ['es-us'],
+      }),
+    ],
   }
 
-  return base;
+  return config
 }
 
-module.exports = configFactory(baseConfig);
+function applyEnv(config) {
+  let newConfig;
+
+  if (nodeEnv === 'production') {
+    newConfig = {
+      ...config,
+      mode: 'production',
+      devtool: 'source-map',
+      optimization: {
+        splitChunks: {
+          cacheGroups: {
+            vendor: {
+              test: /[\\/]node_modules[\\/]/,
+              chunks: 'initial',
+              name: 'javascripts/vendor.js',
+              enforce: true,
+            },
+          },
+        },
+        minimizer: [
+          new TerserPlugin({
+            sourceMap: true,
+            parallel: true,
+          }),
+        ],
+      },
+    };
+  } else {
+    newConfig = {
+      ...config,
+      mode: 'development',
+      devtool: 'cheap-module-eval-source-map',
+    };
+  }
+
+  return newConfig;
+}
+
+function applyLoaders(config) {
+  let newConfig;
+
+  newConfig = {
+    ...config,
+    module: {
+      rules: [{
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+          plugins: [],
+        }
+      }],
+    },
+  };
+
+  return newConfig;
+}
+
+const configurate = flowright(
+  applyLoaders,
+  applyEnv,
+  generateBaseConfig,
+);
+
+module.exports = configurate();
